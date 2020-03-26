d1dbf57d069850475cf10c3619ed6f57fd955d76
sagar-528 <gupta.sagar528@gmail.com>
2020-03-27 02:01:09 +0530

    ADDED: Second Condition

--- pinCode.sh
+++ pinCode.sh
@@ -7,11 +7,15 @@ read -p "Checking the given pin code is valid or not: " pin
 echo " "
 
 ValidatePin="^[0-9]{6}$"
-FailCondition="^([a-zA-Z]*[0-9]){6}*$"
+FailCondition1="^([a-zA-Z]*[0-9]){6}*$"
+FailCondition2="^([a-zA-Z]*[0-9]){6}[a-zA-Z]*$"
 if [[ $pin =~ $ValidatePin ]]
 then
 	echo Valid
-	elif [[ $pin =~ $FailCondition ]]
+	elif [[ $pin =~ $FailCondition1 ]]
+	then
+		echo Fail
+	elif [[ $pin =~ $FailCondition2 ]]
 	then
 		echo Fail
 else
