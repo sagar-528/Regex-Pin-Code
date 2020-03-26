8f1f185853bf5554b3781622a1fd8fdc595b6600
sagar-528 <gupta.sagar528@gmail.com>
2020-03-27 01:46:31 +0530

    ADDED: COndition

--- pinCode.sh
+++ pinCode.sh
@@ -7,10 +7,13 @@ read -p "Checking the given pin code is valid or not: " pin
 echo " "
 
 ValidatePin="^[0-9]{6}$"
-
+FailCondition="^([a-zA-Z]*[0-9]){6}*$"
 if [[ $pin =~ $ValidatePin ]]
 then
-	echo valid
+	echo Valid
+	elif [[ $pin =~ $FailCondition ]]
+	then
+		echo Fail
 else
-	echo invalid
+	echo Invalid
 fi
