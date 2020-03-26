00ce3e195cfb722b064f207cdb15b589b01fc63b
sagar-528 <gupta.sagar528@gmail.com>
2020-03-27 02:54:09 +0530

    ADDED: Modification

--- pinCode.sh
+++ pinCode.sh
@@ -6,16 +6,15 @@
 read -p "Checking the given pin code is valid or not: " pin
 echo " "
 
-ValidatePin="^[0-9]{6}$"
-FailCondition1="^([a-zA-Z]*[0-9]){6}*$"
-FailCondition2="^([a-zA-Z]*[0-9]){6}[a-zA-Z]*$"
+#Validate condition for 6 digit and space between pin code.
+ValidatePin="^([0-9]*\s*[0-9]){6}$"
+
+#Fail condition for pin code.
+FailCondition="^([a-zA-Z]*[0-9]){6}[a-zA-Z]*$"
 if [[ $pin =~ $ValidatePin ]]
 then
 	echo Valid
-	elif [[ $pin =~ $FailCondition1 ]]
-	then
-		echo Fail
-	elif [[ $pin =~ $FailCondition2 ]]
+	elif [[ $pin =~ $FailCondition ]]
 	then
 		echo Fail
 else
